af3998aca3cb5dbe94e1660d47712c75c2d78ed0
Fabrizio <fabrizio.nastri@flexup.org>
2024-08-24 18:22:11 +0200

    create basic book model and first migration

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Book(models.Model):  # extends the Model class
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
