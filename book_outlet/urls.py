56f66fce072e839413e38ee32c6e66688630447d
Fabrizio <fabrizio.nastri@flexup.org>
2024-09-15 17:49:24 +0200

    created generic, sortable table

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="all_books"),
     path("<int:book_id>/", views.book_detail, name="book_detail"),
+    path("generic_table/", views.generic_table_view, name="generic_table_view"),
 ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Book
 
 
@@ -8,6 +8,24 @@ def index(request):
 
 
 def book_detail(request, book_id):
-    return render(
-        request, "book_outlet/book_detail.html", {"book": Book.objects.get(id=book_id)}
-    )
+    # Solution 1 - return standard 404 page
+    # try:
+    #     book = Book.objects.get(id=book_id)
+    # except:
+    #     raise Http404()
+    # Solution 2 - return custom 404 page using built-in shortcut
+    book = get_object_or_404(Book, pk=book_id)  # pk is the primary key, same as id
+    return render(request, "book_outlet/book_detail.html", {"book": book})
+
+
+def generic_table_view(request):
+    columns = [
+        {"name": "title", "title": "Title"},
+        {"name": "author", "title": "Author"},
+        {"name": "rating", "title": "Rating"},
+        {"name": "is_bestselling", "title": "Is best selling ?"},
+    ]
+    data = Book.objects.all()
+    context = {"columns": columns, "data": data}
+
+    return render(request, "book_outlet/generic_table.html", context)
