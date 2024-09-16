7f51d7e47d56b438d8e8573ab2c7329a095c626e
Fabrizio <fabrizio.nastri@flexup.org>
2024-09-16 16:17:06 +0200

    added address one-to-one relationship

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,7 +1,21 @@
 # book_outlet/admin.py
 
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address
+
+
+class AddressAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("street", "country")}
+    list_display = (
+        "author",
+        "street",
+        "city",
+        "state",
+        "postal_code",
+        "country",
+        "slug",
+    )
+    list_filter = ("country",)
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,4 +24,12 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("author", "rating")
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("first_name", "last_name")}
+    list_display = ("full_name", "first_name", "last_name", "books_written", "slug")
+    list_filter = ("first_name", "last_name")
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -6,19 +6,75 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.text import slugify
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=50, null=True)
+    postal_code = models.CharField(max_length=6, null=True)
+    country = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state}, {self.postal_code}, {self.country}"
+
+    def get_absolute_url(self):
+        return reverse("address_detail", args=[self.slug])
+
+    def save(
+        self, *args, **kwargs
+    ):  # not needed if using prepopulated_fields in the admin
+        self.slug = slugify(self)
+        super().save(*args, **kwargs)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50, unique=True)
+    last_name = models.CharField(max_length=50, unique=True)
+    address = models.OneToOneField(
+        Address,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    # returns the number of books written by the author
+    def books_written(self):
+        return self.books.all().count()
+
+    def get_absolute_url(self):
+        return reverse("author_detail", args=[self.slug])
+
+    def save(
+        self, *args, **kwargs
+    ):  # not needed if using prepopulated_fields in the admin
+        self.slug = slugify(self)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Book(models.Model):  # extends the Model class
     title = models.CharField(max_length=50, unique=True)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)], default=0
     )
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books"
+    )
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
 
-    def safe(self, *args, **kwargs):
+    def save(
+        self, *args, **kwargs
+    ):  # not needed if using prepopulated_fields in the admin
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
